87f3e0b82b9f19b48b856cd694e45c1ae66a002f
Sean McCusker <sean.mccusker@gmail.com>
2019-01-21 10:41:42 +1100

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'session/new'
+  # get 'session/new'
   #get 'users/new' # get rid of line
   root :to => 'pages#home'
   resources :users, :only => [:new, :create, :index]
